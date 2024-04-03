8684cd4ceb2beab96ddf003aed8b1491a4af2cd9
GABRIELA MARIA BAGGIO <125301132+BaggioprofGabi23@users.noreply.github.com>
2024-04-03 13:29:55 -0300

    ajustes

--- script.js
+++ script.js
@@ -5,22 +5,53 @@ darkModeBtn.addEventListener('click', function() {
   body.classList.toggle('dark-mode');
 });
 
-const quizForm = document.getElementById('quizForm');
-const resultDisplay = document.getElementById('result');
-
-quizForm.addEventListener('submit', function(event) {
-  event.preventDefault();
-
-  const selectedAnswer = document.querySelector('input[name="answer"]:checked');
+const questions = [
+    { question: 'Qual é a principal causa do aquecimento global?', answer: 'greenhouse' },
+    { question: 'Qual das seguintes atividades contribui para a poluição do ar?', answer: 'deforestation' },
+    { question: 'Qual é a fonte mais comum de poluição plástica nos oceanos?', answer: 'plastic' }
+  ];
   
-  if (selectedAnswer) {
-    const answerValue = selectedAnswer.value;
-    if (answerValue === 'greenhouse') {
-      resultDisplay.textContent = 'Resposta correta! Emissão de gases de efeito estufa é a principal causa do aquecimento global.';
+  const questionContainer = document.getElementById('questionContainer');
+  const quizForm = document.getElementById('quizForm');
+  const scoreContainer = document.getElementById('scoreContainer');
+  const scoreDisplay = document.getElementById('score');
+  let currentQuestionIndex = 0;
+  let score = 0;
+  
+  // Função para exibir a próxima pergunta
+  function showNextQuestion() {
+    if (currentQuestionIndex < questions.length) {
+      const currentQuestion = questions[currentQuestionIndex];
+      document.getElementById('question').textContent = currentQuestion.question;
+      currentQuestionIndex++;
     } else {
-      resultDisplay.textContent = 'Resposta incorreta. Tente novamente.';
+      // Exibe a pontuação final quando não houver mais perguntas
+      questionContainer.style.display = 'none';
+      scoreContainer.style.display = 'block';
+      scoreDisplay.textContent = `Você acertou ${score} de ${questions.length} perguntas.`;
     }
-  } else {
-    resultDisplay.textContent = 'Por favor, selecione uma resposta.';
   }
-});
+  
+  // Função para verificar a resposta e avançar para a próxima pergunta
+  function checkAnswer() {
+    const selectedAnswer = document.querySelector('input[name="answer"]:checked');
+    if (selectedAnswer) {
+      const answerValue = selectedAnswer.value;
+      const currentQuestion = questions[currentQuestionIndex - 1];
+      if (answerValue === currentQuestion.answer) {
+        score++;
+      }
+      selectedAnswer.checked = false;
+      showNextQuestion();
+    }
+  }
+  
+  // Event listener para o formulário de quiz
+  quizForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    checkAnswer();
+  });
+  
+  // Mostra a primeira pergunta quando a página é carregada
+  showNextQuestion();
+  
