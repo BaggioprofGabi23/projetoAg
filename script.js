35d2725a5cf90e57141092a59f813e55698cd215
GABRIELA MARIA BAGGIO <125301132+BaggioprofGabi23@users.noreply.github.com>
2024-04-03 13:27:28 -0300

    ajustes

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const body = document.body;
 darkModeBtn.addEventListener('click', function() {
   body.classList.toggle('dark-mode');
 });
+
+const quizForm = document.getElementById('quizForm');
+const resultDisplay = document.getElementById('result');
+
+quizForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const selectedAnswer = document.querySelector('input[name="answer"]:checked');
+  
+  if (selectedAnswer) {
+    const answerValue = selectedAnswer.value;
+    if (answerValue === 'greenhouse') {
+      resultDisplay.textContent = 'Resposta correta! Emissão de gases de efeito estufa é a principal causa do aquecimento global.';
+    } else {
+      resultDisplay.textContent = 'Resposta incorreta. Tente novamente.';
+    }
+  } else {
+    resultDisplay.textContent = 'Por favor, selecione uma resposta.';
+  }
+});
